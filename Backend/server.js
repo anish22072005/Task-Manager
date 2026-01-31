c718d5b2b90c014bba1da94a78510e3bd1816760
anish22072005 <anishnath2005@gmail.com>
2026-01-31 13:28:03 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const taskRoutes = require("./routes/taskRoutes");
-
 const app = express();
 
 app.use(cors());
